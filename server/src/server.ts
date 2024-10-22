51ce648bf5ac83bc9b9d88cefb5e117837fdfaa0
Keegan Hinton <khinton386@gmail.com>
2024-10-22 19:28:20 -0400

    added in config/database

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,10 @@
 import express, { Application } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import sequelize from '';
+import sequelize from './config/database';
 import thirdPartyRoutes from './api/routes/thirdPartyRoutes';
+import userRoutes from './api/routes/userRoutes';
+import errorHandler from './api/middlewares/errorHandler';
 
 dotenv.config();
 
@@ -11,7 +13,13 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+// Use routes
 app.use('/api', thirdPartyRoutes);
+app.use('/api', userRoutes);
+
+// Error handler should be the last middleware
+app.use(errorHandler);
 
 sequelize.sync()
     .then(() => {
