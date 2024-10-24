c4998f1a748c542cbaaea10f8dde71f47fd7d144
dhurteau26 <dhurteau26@gmail.com>
2024-10-24 19:19:23 -0400

    added navbar and info to main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css'
-import Demo from './navbar.jsx'
+import App from './App.jsx'
+import LandingPage from './landingPage.jsx';
+import Register from './Register.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <LandingPage />,
+      },
+      {
+        path: 'register',
+        element: <Register />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
