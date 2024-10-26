eaa055dbda96bd2685e7d58a11cd06448009018d
Keegan Hinton <khinton386@gmail.com>
2024-10-25 23:47:04 -0400

    added in both the trail api and weather api. finished connection between front end port 3001.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,6 @@
-import express, { Application } from 'express';
+import express, { Application,Request, Response } from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors';
+import cors, { CorsOptions } from 'cors';
 import sequelize from './config/database';
 import thirdPartyRoutes from './api/routes/thirdPartyRoutes';
 import userRoutes from './api/routes/userRoutes';
@@ -13,13 +13,25 @@ dotenv.config();
 const app: Application = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+const corsOptions: CorsOptions ={
+    origin: process.env.CLIENT_URL || 'http://localhost:3001',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    optionsSuccessStatus: 204, 
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
+// Public Routes
 app.use('/api/auth', authRoutes);
 
+// Protected Routes
 app.use('/api/user', authMiddleware, userRoutes);
-app.use('/api', authMiddleware, thirdPartyRoutes);
+app.use('/api', thirdPartyRoutes);
+
+app.get('/api/data', (req: Request, res: Response) => {
+    res.json({ message: 'Yooooo it actually worked' });
+});
 
 app.use(errorHandler);
 
